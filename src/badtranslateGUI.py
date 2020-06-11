f0c3eff94876498a037eba86a1af0de2e03246a5
Calvin Stahoviak <cstahoviak@cs.unm.edu>
2020-06-11 00:11:09 -0600

    ok

--- src/badtranslateGUI.py
+++ src/badtranslateGUI.py
@@ -1,6 +1,11 @@
 import googletrans as g
 import re
 import random
+from spellchecker import SpellChecker
+ 
+spell = SpellChecker(language='en')
+allLanguages = len(g.LANGUAGES)
+t = g.Translator()
 
 print("------------------------------------")
 print("Welcome to the worst translator ever")
@@ -10,10 +15,7 @@ print("[*] To make text go through more languages, enter \"-n\" and the desired 
 print("[*] Otherwise enter text to translate")
 print("------------------------------------")
 
-allLanguages = len(g.LANGUAGES)
-
-t = g.Translator()
-
+# Absolutley destroys your english
 def ruinSentence(string, numRounds):
 
     print("[*] Went through: ", end='')
@@ -36,25 +38,32 @@ def ruinSentence(string, numRounds):
 
     return string
 
+def CorrectSpelling(string):
+    words = spell.split_words(string)
+    [spell.correction(word) for word in words]
+    string = ''.join(string)
 
+    print("After fix")
+    print(string)
+    return string
 
+def main():
+    s = input("[*] Enter number of languages to parse text through: ")
+    numLang = int(s)
 
-s = input("[*] Enter number of languages to parse text through: ")
+    while(True):
+        
+        s = input("-> ")
 
-changeNumLang = False
+        if s == "q":
+            print("[X] Exiting")
+            exit(0)
+        elif s[0:2] == "-n":
+            numLang = int(s[3:])
+            print("[*] New Number is {}".format(numLang))
+        else:
+            s = CorrectSpelling(s)
+            print("[*] Result: {}".format(ruinSentence(s, numLang)))
 
-numLang = int(s)
-
-while(True):
-    
-    s = input("-> ")
-
-    if s == "q":
-        print("[X] Exiting")
-        exit(0)
-    elif s[0:2] == "-n":
-        numLang = int(s[3:])
-        print("[*] New Number is {}".format(numLang))
-    else:
-
-        print("[*] Result: {}".format(ruinSentence(s, numLang)))
+if __name__ == "__main__":
+    main()
